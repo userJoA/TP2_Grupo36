c4eb49564b95460c2f14478105a5666980ea0a19
desc <desc@gato-pc>
2022-04-17 19:14:59 -0300

    secuencia agregada

--- TP2_Grupo36/src/main/java/ar/edu/unju/fi/punto7/SecuenciaNumerica.java
+++ TP2_Grupo36/src/main/java/ar/edu/unju/fi/punto7/SecuenciaNumerica.java
@@ -1,5 +1,16 @@
 package ar.edu.unju.fi.punto7;
 
 public class SecuenciaNumerica {
-
+	public static void main(String args[])
+    {
+    	int i=2;
+    	int j=40;
+    	
+        for(j=j;j>=0;j--)
+        {
+        		System.out.println(j*i);
+        		i++;
+         }
+        System.out.println("Valor de j: " + j);
+    }
 }
